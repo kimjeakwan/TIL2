8b8e2482c6d79aa7539af60c56ce8b43bc803fdd
kimjeakwan <s24042@gsm.hs.kr>
2024-05-30 23:46:05 +0900

    backend

--- src/main/java/spring4/spring4/Spring4Application.java
+++ src/main/java/spring4/spring4/Spring4Application.java
@@ -1,1 +1,15 @@
-dddd
+package spring4.spring4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4Application.class, args);
+	}
+
+
+
+}
